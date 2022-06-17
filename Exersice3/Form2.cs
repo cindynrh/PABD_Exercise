fca616c68c5355ba3ef5ee2fa2c80b8150902f24
cindynrh <cindynurhabibah@gmail.com>
2022-06-17 13:39:10 +0700

    Add project files.

---
+++ Exersice3/Form2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exersice3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxEmp.Text))
+            {
+                MessageBox.Show("Nama : ");
+                textBoxEmp.Select();
+            }
+            else if (string.IsNullOrWhiteSpace(textBoxCity.Text))
+            {
+                MessageBox.Show("Password : ");
+                textBoxCity.Select();
+            }
+            else if (string.IsNullOrWhiteSpace(textBoxDept.Text))
+            {
+                MessageBox.Show("No. Telepon : ");
+                textBoxDept.Select();
+            }
+            else
+            {
+                try
+                {
+                    if (sqlCon.State == ConnectionState.Closed)
+                    {
+                        sqlCon.Open();
+                    }
+                    DataTable dtData = new DataTable();
+                    sqlCmd = new SqlCommand("spEmployee", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@ActionType", "SaveData");
+                    sqlCmd.Parameters.AddWithValue("@Nama :", EmployeeId);
+                    sqlCmd.Parameters.AddWithValue("@Password :", textBoxEmp.Text);
+                    sqlCmd.Parameters.AddWithValue("@No. Telepon : ", textBoxCity.Text);
+
+                    int numRes = sqlCmd.ExecuteNonQuery();
+                    if (numRes > 0)
+                    {
+                        MessageBox.Show("Record Saved Successfully !!!");
+                        ClearAllData();
+                    }
+                    else
+                        MessageBox.Show("Please Try Again !!!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:- " + ex.Message);
+                }
+            }
+        }
+    }
+}
